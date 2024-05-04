e0659b7e87bdc08ed3d5ec72c456947055e688ca
klassenserver7b <klassenserver7bwin10@gmail.com>
2024-05-04 21:18:20 +0200

    GameWindow

--- src/main/java/GameWindow.java
+++ src/main/java/GameWindow.java
@@ -1,2 +1,36 @@
-public class GameWindow {
+import processing.core.PApplet;
+
+public class GameWindow extends PApplet {
+
+    private static GameWindow INSTANCE = null;
+
+    public static GameWindow getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new GameWindow();
+        }
+
+        return INSTANCE;
+    }
+
+    public GameWindow() {
+        INSTANCE = this;
+    }
+
+    @Override
+    public void settings() {
+        size(1000, 1000);
+    }
+
+    @Override
+    public void setup() {
+        surface.setResizable(true);
+        noStroke();
+        background(0);
+        frameRate(30);
+    }
+
+    @Override
+    public void draw() {
+    }
+
 }
